9509d0d7edf1c49eb1765766e27fa43b008cfbb1
James Socol <james@mozilla.com>
2011-03-29 19:52:06 -0400

    Be smarter if there's a "/" in the destination.

--- data/js/shortcuts.js
+++ data/js/shortcuts.js
@@ -16,8 +16,16 @@ var SHORTCUTS = {
             input.style.cssText = "display: block; position: absolute; top: 0; left: 0";
             input.addEventListener("keyup", function(e) {
                 if (e.keyCode == 13) {  // 13 = Enter
-                    var path = "/" + githubUser() + "/" + this.value;
-                    window.location = path;
+                    if (-1 != this.value.indexOf('/')) {
+                        if (-1 != this.value.search(/^\//)) {
+                            window.location = this.value;
+                        } else {
+                            window.location = "/" + this.value;
+                        }
+                    } else {
+                        var path = "/" + githubUser() + "/" + this.value;
+                        window.location = path;
+                    }
                 } else if (e.keyCode == 27) {  // 27 = Escape
                     this.blur();
                     document.body.removeChild(this);
